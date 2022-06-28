7d54532b867477af119827909a648045c5cf8b56
Brodie Friday <brodieleefriday@gmail.com>
2022-06-28 13:03:32 -0500

    Added difficulty level feature to program and one more question

--- RockRacer.py
+++ RockRacer.py
@@ -1,5 +1,5 @@
-
-
+import random
+from sre_parse import WHITESPACE 
 P0 = ("""       
                    _                                   _                                     _                         /|
                   /|\\                                 /|\                                   /|\\                        \|FINISH
@@ -79,47 +79,82 @@ ___________________|___________________________________|________________________
           
           
                        """) 
-
 print("pick a difficulty: Expert or Beginner \n Type 'Expert' or 'Beginner'") #User picks their difficulty
 difficulty = input()
 yourScore = "Your Score: "
 score = 0
+while(difficulty != "Expert" or difficulty !="Beginner"):
+  if(difficulty == "Expert" or difficulty == "Beginner"):
+    break
+  print("pick a difficulty: Expert or Beginner \n Type 'Expert' or 'Beginner'") #User picks their difficulty
+  difficulty = input()
 if(difficulty == "Expert"):
+  question1Number1 = random.randint(6,40)
+  question1Number2 = random.randint(6,40)
+  question2Number1 = random.randint(6,40)
+  question2Number2 = random.randint(6,40)
+  question3Number1 = random.randint(6,40)
+  question3Number2 = random.randint(6,40)
+  question4Number1 = random.randint(6,40)
+  question4Number2 = random.randint(6,40)
+  question5Number1 = random.randint(6,40)
+  question5Number2 = random.randint(6,40)
+  question6Number1 = random.randint(6,40)
+  question6Number2 = random.randint(6,40)
   print(P0)
-  print("Okay, your difficulty level is set. Once you begin you will be prompted to answer various mathmatical questions until your rock as crossed the finsih line.\n Type 'START' to begin.")
-  start = input()
-  if(start == "START"):
-    userAnswer1 = 0
-    answer1 = 4
-    # Question 1
-    while(userAnswer1 != answer1):
-      print("What is 2 + 2?")
-      try:
-        userAnswer1 = input()
-        userAnswer1 = float(userAnswer1)
-      except:
-        print("Value entered must be an integer.")
-      if(userAnswer1 == answer1):
-        score = score + 1
-        print()
-        print(yourScore + str(score))
-        print("Correct! <--ADVANCED-->")
-        print(P1)
-        print()
-      if(userAnswer1!= answer1):
-        score = score + 1
-        print()
-        print(yourScore + str(score))
-        print("Incorrect! <--STAY-->")
-        print(P0)
-        print()
+  print("Okay, your difficulty level is set to expert. Once you begin you will be prompted to answer various mathmatical questions until your rock as crossed the finsih line.\n Type 'START' to begin.")
+elif(difficulty == "Beginner"):
+  question1Number1 = random.randint(0,15)
+  question1Number2 = random.randint(0,15)
+  question2Number1 = random.randint(0,15)
+  question2Number2 = random.randint(0,15)
+  question3Number1 = random.randint(0,15)
+  question3Number2 = random.randint(0,15)
+  question4Number1 = random.randint(0,15)
+  question4Number2 = random.randint(0,15)
+  question5Number1 = random.randint(0,15)
+  question5Number2 = random.randint(0,15)
+  question6Number1 = random.randint(0,15)
+  question6Number2 = random.randint(0,15)
+  print(P0)
+  print("Okay, your difficulty level is set to beginner. Once you begin you will be prompted to answer various mathmatical questions until your rock as crossed the finsih line.\n Type 'START' to begin.")
+  
+start = input() 
+if(start == "START"):
+  userAnswer1 = 0
+  answer1 = question1Number1 * question1Number2
+  question1 = "What is " + str(question1Number1) + " * " + str(question1Number2) + "?"
+
+  # Question 1
+  while(userAnswer1 != answer1):
+    print(question1)
+    try:
+      userAnswer1 = input()
+      userAnswer1 = float(userAnswer1)
+    except:
+      print("Value entered must be an integer.")
+    if(userAnswer1 == answer1):
+      score = score + 1
+      print()
+      print(yourScore + str(score))
+      print("Correct! <--ADVANCED-->")
+      print(P1)
+      print()
+    if(userAnswer1!= answer1):
+      score = score + 1
+      print()
+      print(yourScore + str(score))
+      print("Incorrect! <--STAY-->")
+      print(P0)
+      print()
     
 
  # Question 2
-  answer2 = 8
   userAnswer2 = 0
+  answer2 = question2Number1 * question2Number2
+  question2 = "What is " + str(question2Number1) + " * " + str(question2Number2) + "?"
   while(userAnswer2 != answer2):
-    print("What is 4 + 4?") # Question 2
+    print(question2) 
     try:
       userAnswer2 = input()
       userAnswer2 = float(userAnswer2)
@@ -140,10 +175,11 @@ if(difficulty == "Expert"):
       print()
 
   # Question 3
-  answer3 = 16
   userAnswer3 = 0
+  answer3 = question3Number1 * question3Number2
+  question3 = "What is " + str(question3Number1) + " * " + str(question3Number2) + "?"
   while(userAnswer3 != answer3):
-    print("What is 8 + 8?") # Question 2
+    print(question3) 
     try:
       userAnswer3 = input()
       userAnswer3 = float(userAnswer3)
@@ -165,10 +201,11 @@ if(difficulty == "Expert"):
   
 
   # Question 4
-  answer4 = 20
   userAnswer4 = 0
+  answer4 = question4Number1 * question4Number2 + question4Number1
+  question4 = "What is " + str(question4Number1) + " * " + str(question4Number2) + " + " + str(question4Number1) + "?"
   while(userAnswer4 != answer4):
-    print("What is 10 + 10?") # Question 2
+    print(question4)
     try:
       userAnswer4 = input()
       userAnswer4 = float(userAnswer4)
@@ -190,10 +227,11 @@ if(difficulty == "Expert"):
   
 
   # Question 5
-  answer5 = 30
   userAnswer5 = 0
+  answer5 = question5Number1 * question5Number2
+  question5 = "What is " + str(question5Number1) + " * " + str(question5Number2) + "?"
   while(userAnswer5 != answer5):
-    print("What is 15 + 15?") # Question 2
+    print(question5)
     try:
       userAnswer5 = input()
       userAnswer5 = float(userAnswer5)
@@ -213,4 +251,30 @@ if(difficulty == "Expert"):
       print(P4)
       print()
 
-print("Your Score: " + str(score))
+  # Question 6
+  userAnswer6 = 0
+  answer6 = question6Number1 * question6Number2 - question6Number2
+  question6 = "What is " + str(question6Number1) + " * " + str(question6Number2) + " - " + str(question6Number2) + "?"
+  while(question6):
+    print(question6) 
+    try:
+      userAnswer6 = input()
+      userAnswer6 = float(userAnswer6)
+    except:
+      print("Value entered must be an integer.")
+    if(userAnswer6 == answer6):
+      score = score + 1
+      print()
+      print(yourScore + str(score))
+      print("Correct! <--ADVANCED-->")
+      print(P6)
+      print()
+      print("Your Score: " + str(score))
+      print("CONGRATULATIONS!! YOU HAVE COMPLETED THE COURSE.")
+      quit()
+    if(userAnswer6 != answer6):
+      score = score + 1
+      print(yourScore + str(score))
+      print("Incorrect! <--STAY-->")
+      print(P5)
+      print()
